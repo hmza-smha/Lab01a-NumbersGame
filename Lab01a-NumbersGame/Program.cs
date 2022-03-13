36a41b2fe0a36dead8cff9e1bf72d31a4ccc7b49
HamZa Samha <smha.hmza@gmail.com>
2022-03-13 20:50:29 +0300

    Add project files.

---
+++ Lab01a-NumbersGame/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Lab01a_NumbersGame
+{
+    class Program
+    {
+        static void StartSequence()
+        {
+            try
+            {
+                Console.WriteLine("Enter an integer number greater than 0 : ");
+                String input = Console.ReadLine();
+                int num = Convert.ToInt32(input);
+                int[] arr = new int[num];
+                Populate(arr);
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("Array Size: " + num);
+                Console.WriteLine("Array Elements ");
+                foreach (int e in arr)
+                {
+                    Console.WriteLine(e);
+                }
+                Console.WriteLine("------------------------------");
+                int sum = GetSum(arr);
+                Console.WriteLine("Array Sum is: " + sum);
+                Console.WriteLine("------------------------------");
+                int product = GetProduct(arr, sum);
+                Console.WriteLine("Array Product is: " + product);
+                Console.WriteLine("------------------------------");
+                decimal co = GetQuotient(product);
+                Console.WriteLine("Array Quotient is: " + co);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong! " + e);
+            }
+
+            
+        }
+
+        static int[] Populate(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter number " + (i + 1) + "/" + (arr.Length));
+                String input = Console.ReadLine();
+                arr[i] = Convert.ToInt32(input);
+            }
+
+            return arr;
+        }
+
+        static int GetSum(int[] arr)
+        {
+            int sum = 0;
+
+            foreach (int e in arr)
+            {
+                sum += e;
+            }
+
+            if (sum < 20)
+            {
+                throw new Exception("Value is less than 20");
+            }
+
+            return sum;
+        }
+
+        static int GetProduct(int[] arr, int sum)
+        {
+            int num = -1;
+            Console.WriteLine("Pick a random number between 1 and " + arr.Length);
+            String input = Console.ReadLine();
+            
+            try
+            {
+                num = Convert.ToInt32(input);
+                if(num < 1 || num > arr.Length)
+                {
+                    throw new IndexOutOfRange();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            int product = sum * num;
+
+            return product;
+        }
+
+        static decimal GetQuotient(int product)
+        {
+            Console.WriteLine("Enter number to divide by " + product + " :");
+            String input = Console.ReadLine();
+            decimal result = 0;
+            try
+            {
+                int num = Convert.ToInt32(input);
+                result = Decimal.Divide(product, num);
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("Cannot divide on Zero!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something went wrong! " + e);
+            }
+
+            return result;            
+        }
+
+        static void Main(string[] args)
+        {
+            StartSequence();
+        }
+    }
+
+    [Serializable]
+    internal class IndexOutOfRange : Exception
+    {
+        public IndexOutOfRange()
+        {
+        }
+
+        public IndexOutOfRange(string message) : base(message)
+        {
+        }
+
+        public IndexOutOfRange(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected IndexOutOfRange(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Lab01a-NumbersGame/README.md
@@ -0,0 +1,22 @@
+﻿# Overview
+
+This program calculates 
+- Sum
+- Product
+- Quotient
+
+## Sum (array)
+- sum all the elements in the array
+	- if the sum less than 20 it will throw an exception
+- return the sum of the array
+
+## Product (array, sum)
+- the user picks a random number between 1 and array length
+	- if the user inter number out of the range then the method will throw IndexOutOfRange
+	- if the user input a not valid number, the method will throw exceptoin
+- return sum * picked number
+
+## Quotient (product)
+- the user picks a number to divide by product
+	- if the picked number is 0 it will throw an exeption
+- return Decimal.Divide(product, num)
